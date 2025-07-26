fbde660c465935377bd9e9f558782482e2b3110f
Dhayanandhan GK <dhayadharneesh@gmail.com>
2025-07-26 09:58:06 +0530

    Update countvowels_consonants.java

--- countvowels_consonants.java
+++ countvowels_consonants.java
@@ -1,25 +1,22 @@
-package com.dhaya;
 import java.util.*;
-public class countvowels_consonants {
-
+class countvowels_consonants
+{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
-		int vowels=0,con=0;
-		for(int i=0;i<str.length();i++) {
-			if(str.charAt(i)>='A'&&str.charAt(i)<='Z'||str.charAt(i)>='a'&&str.charAt(i)<='z')
-			if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'||str.charAt(i)=='A'||str.charAt(i)=='E'||str.charAt(i)=='I'||str.charAt(i)=='O'||str.charAt(i)=='U') {
-				vowels++;
-			}else {
-				con++;
-			}
-			
-		}
+		int c=0,v=0;
+		for(int i=0;i<str.length();i++){
+		    char ch=str.charAt(i);
+		    if(Character.isLetter(ch)){
+		        char l=Character.toLowerCase(ch);
+		        if(l=='a'|| l=='e'||l=='i'||l=='o'||l=='u'){
+		            v++;
+		        }else{
+		            c++;
+		        }
+		    }
+		}System.out.println(v);
+		System.out.println(c);
 		
-		System.out.println("Vowels count : "+vowels);
-		System.out.println("Consonants count : "+con);
-
-	}
-
+		}
 }
